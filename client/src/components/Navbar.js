0be0c5047af9d77792293dc79d17425021b8f541
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-07 21:48:05 +0300

    Linked other pages

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <header>
+       <div class='menubar'>
+       <Link to='/' style={{ color: '#3aeaca', textDecoration: 'none' }}><h1>By design Nail Parlour</h1></Link>
+       <nav>
+          <ul>
+            <li>
+              <NavLink
+                exact
+                to='/'
+                className={({ isActive }) =>
+                  isActive ? 'link active' : 'link'
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                to='/services'
+                className={({ isActive }) =>
+                  isActive ? 'link active' : 'link'
+                }
+              >
+                Our Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                exact
+                to='/manicurists'
+                className={({ isActive }) =>
+                  isActive ? 'link active' : 'link'
+                }
+              >
+                Manicurists
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/contactus'
+                exact
+                className={({ isActive }) =>
+                  isActive ? 'link active' : 'link'
+                }
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Link exact to="/Signup" id='menu-login-link'>
+          <button type='button' id='menu-login-button'>
+            Sign Up
+          </button>
+        </Link>
+       </div>
+    </header>
+  )
+}
+
+export default Navbar
