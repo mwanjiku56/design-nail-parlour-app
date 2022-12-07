e94688b48fe7b6e98b2b4d561cd983a4e08de3e0
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-07 07:51:47 +0300

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :appointments, only:[:index, :show, :create] 
+  resources :manicurists, only: :index
+  resources :reviews, only:[:update, :index, :show, :create]
+  resources :customers
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
