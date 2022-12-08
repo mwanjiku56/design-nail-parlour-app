df607d056d339488fdf9297d24bb75143b6d08a7
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-08 14:38:07 +0300

    added a table

--- client/src/components/Manicurist.js
+++ client/src/components/Manicurist.js
@@ -1,24 +1,43 @@
-import React,{useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from "react";
 
 function Manicurist() {
-    const [manicurists, setManicurists] = useState([]);
+  const [manicurists, setManicurists] = useState([]);
 
-    useEffect(() => {
-        fetch("/manicurists")
-          .then((response) => response.json())
-          .then(data => setManicurists(data))
-      }, []);
-      
-      console.log(manicurists);
+  useEffect(() => {
+    fetch("/manicurists")
+      .then((response) => response.json())
+      .then((data) => setManicurists(data));
+  }, []);
   return (
-    <div className='trainers-container'>
-      <h1 className='trainers-heading'>Meet the Manicurist Team</h1>
-      <div className='trainers'></div>
-      <div className='join-us'>
-      </div>
+    <div className="trainers-container">
+      <h1 className="trainers-heading">Meet the Manicurist Team</h1>
+
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">App ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Job Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {manicurists.length < 1 ? (
+            <tr>
+              <td colSpan="3">No Manicurist Available</td>
+            </tr>
+          ) : (
+            manicurists.map((app) => (
+              <tr>
+                <td key={app.id}>{app.id}</td>
+                <td>{app.name}</td>
+                <td>{app.job_title}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Manicurist
+export default Manicurist;
