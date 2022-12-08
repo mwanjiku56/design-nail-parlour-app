cdb59934bf848014bcc4a2e9c13261477193f780
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-09 01:26:28 +0300

    created routes to sign up ,login and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   
-  resources :appointments, only:[:index, :show, :create] 
-  resources :manicurists, only: :index
-  resources :reviews, only:[:update, :index, :show, :create]
-  resources :customers
+  
+  resources :appointments
+  resources :manicurists, only: [:index, :create]
+  resources :reviews, only:[:index, :show, :create, :update]
+  resources :customers, only:[:index, :show, :create]
+
+  post "/signup", to: "customers#create"
+
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
