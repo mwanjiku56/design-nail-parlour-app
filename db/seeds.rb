4089dbc25dd6642db2119edcebe687e088ae9c6a
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-09 17:40:34 +0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,50 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-    # 5.times do
-    #     Customer.create(
-    #         first_name: "Martha",  
-    #         email: "user1@google.com", 
-    #         password: "sinaUbaya",  
-    #     )
-    # end
-
-# Customer.create(first_name: "Martha", last_name: "Brooke", email: "user1@google.com", password: "sinaUbaya", confirm_password: "sinaUbaya", manicurist_id: 5)
-# Customer.create(first_name: "Chris", last_name: "Johnson", email: "user2@google.com", password: "sinaUbaya", confirm_password: "sinaUbaya", manicurist_id: 4)
-# Customer.create(first_name: "Lionel", last_name: "Messi", email: "user3@google.com", password: "sinaUbaya", confirm_password: "sinaUbaya", manicurist_id: 3)
-# Customer.create(first_name: "Kylian", last_name: "Mbappe", email: "user4@google.com", password: "sinaUbaya", confirm_password: "sinaUbaya", manicurist_id: 2)
-# Customer.create(first_name: "Romelu", last_name: "Lukaku", email: "user5@google.com", password: "sinaUbaya", confirm_password: "sinaUbaya", manicurist_id: 1)
-# Customer.create(first_name: "Mercy", last_name: "Wanjiku", email: "user6@google.com", password: "sinaUbaya", confirm_password: "sinaUbaya", manicurist_id: 2)
     
-    # 5.times do
-    #     Manicurist.create(
-    #         name: "Bob", 
-    #         job_title: "junior manicurist", 
-    #         image: "nice"
-    #     )
-    # end
+    
+    
+    Manicurist.create(name: "Bob", job_title: "junior manicurist")
+    Manicurist.create(name: "Isaac", job_title: "senior manicurist")
+    Manicurist.create(name: "marley", job_title: "manager ")
+    Manicurist.create(name: "Triza", job_title: "trainee manicurist")
+    Manicurist.create(name: "guru", job_title: "junior manicurist")
+    Manicurist.create(name: "nice", job_title: "senior manicurist")
 
-    # 5.times do
-    #     Review.create( 
-    #         review_note: "great service",
-    #         customer_id: 1,
-    #         Appointment: 4,
-    #         manicurist_id
-    #     ) 
-    # end
 
-# Review.create(customer_id: 1, review_note: "great service")
-# Review.create(customer_id: 2, review_note: "the service took longer today but i loved the outcome")
-# 5.times do
-#     Appointment.create(
-#         customer_id: 1, 
-#         nail_design:"french tips", 
-#         date: 10/12/22,  
-#         manicurist_id: 1
-#     )
-# end
-# Appointment.create(customer_id: 1, first_name: "lionel",last_name:"Messi",nail_design:"french tips", date: 10/12/22,  manicurist_id: 1)
-# Appointment.create(customer_id: 2, first_name: "mercy", last_name:"Messi",nail_design:"coffin design", date: 12/11/22,  manicurist_id: 5)
-# Appointment.create(customer_id: 3, first_name: "mbappe", last_name:"Kylian",nail_design:"overlays", date: 13/11/22,  manicurist_id: 4)
-# Appointment.create(customer_id: 4, first_name: "mercy", last_name:"Wanjiku",nail_design:"stelletos", date: 20/12/22,  manicurist_id: 3)
-# Appointment.create(customer_id: 5, first_name: "martha", last_name:"Brooke",nail_design:"natural", date: 14/11/22,  manicurist_id: 2)
+
+   Customer.create(first_name: "Martha",  email: "user@google.com", password: "sinaUbaya")
+   Customer.create(first_name: "joy",  email: "user1@google.com", password: "sinaUBaya")
+   Customer.create(first_name: "Mercy",  email: "user2@google.com", password: "siNaUbaya")
+   Customer.create(first_name: "Melissa",  email: "user3@google.com", password: "sinaUbaYa")
+   Customer.create(first_name: "Melvin",  email: "user4@google.com", password: "sinaUbAya")
+
+
+    Appointment.create(customer_id: 1,nail_design:"french tips", date: "10/12/22",  manicurist_id: 1)
+    Appointment.create(customer_id: 2, nail_design:"coffin design", date: "12/11/22",  manicurist_id: 5)
+    Appointment.create(customer_id: 3, nail_design:"overlays", date: "13/11/22",  manicurist_id: 4)
+    Appointment.create(customer_id: 4, nail_design:"stelletos", date: "20/12/22",  manicurist_id: 3)
+    Appointment.create(customer_id: 5, nail_design:"natural", date: "14/11/22",  manicurist_id: 2)
+
+
+    Review.create( review_note: "great service",customer_id: 2,appointment_id: 2)
+    Review.create( review_note: "the service took longer today ",customer_id: 2,appointment_id: 1)
+    Review.create( review_note: "great service,thankyou",customer_id: 4,appointment_id: 3)
+    Review.create( review_note: "the service took longer today but i loved the outcome",customer_id: 5,appointment_id: 4)
+
+
+
+
+
