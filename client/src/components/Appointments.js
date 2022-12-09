6aaadc86e02a0c3fa99bb46dfd7be203ca26c144
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-09 23:41:04 +0300

    added an update and delete button

--- client/src/components/Appointments.js
+++ client/src/components/Appointments.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
-import "./Appointment.css";
+// import "./Appointment.css";
 
 function Appointments() {
   const [appoint, setAppoint] = useState([]);
 
-  const [id, setId] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [success, setSuccess] = useState("");
@@ -20,33 +18,6 @@ function Appointments() {
       .then((data) => setAppoint(data));
   }, []);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        password,
-        email,
-      }),
-    }).then((r) => {
-      setIsLoading(false);
-      if (r.ok) {
-        r.json().then((user) => {
-          setSuccess(user);
-          navigate("/appointment");
-        });
-      } else {
-        r.json().then((err) => {
-          setErrors(err.errors);
-          console.log(err);
-        });
-      }
-    });
-  }
-
   return (
     <div className="row">
       <div className="card bg-light col-12">
@@ -54,14 +25,20 @@ function Appointments() {
           <h4 className="card-title mt-3 text-center">Appointment</h4>
         </article>
 
-        <table className="table">
+        <table className="table" table-stripped>
           <thead className="thead-dark">
             <tr>
               <th scope="col">Customer ID</th>
               <th scope="col">Date</th>
               <th scope="col">Nail Design</th>
               <th scope="col">Manicurist ID</th>
-              <th scope="col"></th>
+              <th scope="col">
+                <Link to="/create-appointment">
+                  <button type="submit" className="btn btn-success">
+                    Book Appointment
+                  </button>
+                </Link>
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -81,12 +58,7 @@ function Appointments() {
                     <div className="row">
                       <div className="col-sm-6">
                         <form>
-                          <input
-                            type="hidden"
-                            id="id"
-                            value={data.id}
-                            onChange={(e) => setId(e.target.value)}
-                          />
+                          <input type="hidden" id="id" />
                           <button type="submit" className="btn btn-success">
                             Update
                           </button>
@@ -94,12 +66,7 @@ function Appointments() {
                       </div>
                       <div className="col-sm-6">
                         <form>
-                          <input
-                            type="hidden"
-                            id="id"
-                            value={data.id}
-                            onChange={(e) => setId(e.target.value)}
-                          />
+                          <input type="hidden" id="id" />
                           <button type="submit" className="btn btn-danger">
                             Delete
                           </button>
